87de75694d13729c41e7e51f6396492f96f894de
Lucas Zunho <lucaszunho17@gmail.com>
2022-09-29 09:27:35 -0300

    Definições de bibliotecas necessárias e ajustamento do CMake (diretório include)

--- configure.sh
+++ configure.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
 # -*- Coding: UTF-8 -*-
 
-# Download das bibliotecas necessárias
-echo "~> Libraries download"
-python3 pkgsdownload.py
+PROJECT_PATH = `pwd`
+BUILD_PKGS = 0
+
+if [ "$1" == "--build" ]; then
+    BUILD_PKGS = 1
+fi
+
+# Bibliotecas necessárias
+if [ BUILD_PKGS ]; then
+    cd $PROJECT_PATH/include/libcyaml-1.3.1/
+    make
+    
+else
+    mkdir -p include/
+    cd include/
+    while IFS='=>' read line; do 
+        pkg_filename = line[0]
+        pkg_url = line[1]
+        wget pkg_url -O pkg_filename
+        unzip pkg_filename
+    done < $PROJECT_PATH/packages.txt
+fi
--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <cyaml/cyaml.h>
+
 int main(int argc, char** argv) {
     printf("Hello, CMake!");
     return 0;
