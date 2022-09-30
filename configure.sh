43679de0ba62170cc21fdde5b9603b937aeb39f6
Lucas Zunho <lucaszunho17@gmail.com>
2022-09-30 19:47:31 -0300

    Váriaveis do script de configuração configuradas corretamente && Adicionada configuração do Git Ignore para as libraries

--- configure.sh
+++ configure.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 # -*- Coding: UTF-8 -*-
 
-PROJECT_PATH = `pwd`
-BUILD_PKGS = 0
+PROJECT_PATH=`pwd`
+BUILD_PKGS=0
 
 if [ "$1" == "--build" ]; then
-    BUILD_PKGS = 1
+    $BUILD_PKGS=1
 fi
 
 # Bibliotecas necessárias
 if [ BUILD_PKGS ]; then
-    libcyaml_root = $PROJECT_PATH/packages/libcyaml-1.3.1
+    libcyaml_root=$PROJECT_PATH/packages/libcyaml-1.3.1
     cd $libcyaml_root
     make
     mkdir -p $PROJECT_PATH/lib/libcyaml/lib
     mkdir -p $PROJECT_PATH/lib/libcyaml/include
     cp $libcyaml_root/build/release/libcyaml.a $PROJECT_PATH/lib/libcyaml/lib/
-    cp $libcyaml_root/include/cyaml/ $PROJECT_PATH/lib/libcyaml/include/
+    cp -r $libcyaml_root/include/cyaml/ $PROJECT_PATH/lib/libcyaml/include/
 fi
