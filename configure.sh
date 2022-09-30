58d0e59af4c60a3782c6667a8316e34a558b73d0
Lucas Zunho <lucaszunho17@gmail.com>
2022-09-30 18:32:37 -0300

    Adicionando suporte estático para a biblioteca cyaml

--- configure.sh
+++ configure.sh
@@ -12,14 +12,4 @@ fi
 if [ BUILD_PKGS ]; then
     cd $PROJECT_PATH/include/libcyaml-1.3.1/
     make
-    
-else
-    mkdir -p include/
-    cd include/
-    while IFS='=>' read line; do 
-        pkg_filename = line[0]
-        pkg_url = line[1]
-        wget pkg_url -O pkg_filename
-        unzip pkg_filename
-    done < $PROJECT_PATH/packages.txt
 fi
--- src/main.c
+++ src/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <cyaml/cyaml.h>
+
 
 int main(int argc, char** argv) {
     printf("Hello, CMake!");
