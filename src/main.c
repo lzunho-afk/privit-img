db642961275e84077dc9c1cc7cb904fa319ae96e
Lucas Zunho <lucaszunho17@gmail.com>
2022-10-02 16:04:30 -0300

    Introdução dos headers (Banco de dados & argp)

--- src/main.c
+++ src/main.c
@@ -3,7 +3,17 @@
 
 #include "cyaml/cyaml.h"
 
+#include <argp.h>
+#include <sqlite3.h>
+
+#include "config.h"
+#include "version.h"
+
+#ifdef POSTGRESQL_SUPPORT
+#include <libpq-fe.h>
+#endif
+
 int main(int argc, char** argv) {
-    printf("Hello, CMake!");
+    printf("Hello, CMake!\n");
     return 0;
 }
